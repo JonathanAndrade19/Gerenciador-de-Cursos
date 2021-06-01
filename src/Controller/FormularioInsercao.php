986f36c10b22adef3774be70efd24074b5b793fa
JonathanAndrade19 <jonathanjampamb@gmail.com>
2021-05-31 21:43:36 -0300

    new files header and footer

--- src/Controller/FormularioInsercao.php
+++ src/Controller/FormularioInsercao.php
@@ -13,6 +13,7 @@ class FormularioInsercao implements InterfaceControladorRequisicao
 {
     public function processaRequisicao(): void
     {
+        $titulo = 'Novo Curso';
         require __DIR__ . '/../../view/cursos/formulario.php';
     }
 }
